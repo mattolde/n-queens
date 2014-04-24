358245646daacb2a31f6dde3df905076ccfd2250
Hack Reactor Students <mattolde@gmail.com>
2014-04-23 21:40:33 -0700

    solved n queens!

--- src/solvers.js
+++ src/solvers.js
@@ -35,7 +35,7 @@ window.findNRooksSolution = function(n) {
 
   var solution = board.rows();
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
 
   return solution;
 };
@@ -62,7 +62,7 @@ window.countNRooksSolutions = function(n) {
 
   checkBoard(new Board({n: n}), 0);
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
@@ -70,7 +70,27 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  // var makeEmptyMatrix
+  var board = new Board({n: n});
+  var rows = board.rows();
+
+  // interate through rows
+  for(var row = 0; row < rows.length; row++){
+    // interate through cols
+    for(var col = 0; col < rows.length; col++){
+      // place piece at row col index
+      board.togglePiece(row, col);
+      // if row or col conflict
+      if(board.hasRowConflictAt(row) || board.hasColConflictAt(col) || board.hasAnyMinorDiagonalConflicts() || board.hasAnyMajorDiagonalConflicts()){
+        // remove the piece
+        board.togglePiece(row, col);
+      }
+    }
+  }
+
+  var solution = board.rows();
+
+  // var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -79,8 +99,25 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+
+  var solutionCount = 0;
+
+  var checkBoard = function(board, row){
+    for(var col = 0; col < n; col++){
+      board.togglePiece(row, col);
+      if(!board.hasRowConflictAt(row) && !board.hasColConflictAt(col) && !board.hasAnyMinorDiagonalConflicts() && !board.hasAnyMajorDiagonalConflicts()){
+        if(row === (n - 1)){
+          solutionCount++;
+        } else {
+          checkBoard(board, row + 1);
+        }
+      }
+      board.togglePiece(row, col);
+    }
+  };
+
+  checkBoard(new Board({n: n}), 0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  return n === 0 ? 1 : solutionCount; // edit me later
 };
